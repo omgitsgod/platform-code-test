9b93b828ba8186b82f44f61c00fd5eb0c6ed57b5
omgitsgod <omgitsgod@gmail.com>
2019-07-01 19:45:53 -0400

    Add Blue Star conditions

--- update_quality.rb
+++ update_quality.rb
@@ -4,6 +4,7 @@ def update_quality(awards)
   awards.each do |award|
     case
       when award.name == 'NORMAL ITEM' && award.quality > 0 then award.quality -= 1
+      when award.name == 'Blue Star' && award.quality > 0 then award.quality -= 2
       when award.name == 'Blue First' && award.quality < 50 then award.quality += 1
       when award.name == 'Blue Compare' && award.expires_in > 10 && award.quality < 50 then award.quality += 1
       when award.name == 'Blue Compare' && award.expires_in < 6 && award.quality < 50 then award.quality += 3
@@ -16,6 +17,7 @@ def update_quality(awards)
       when award.expires_in < 0 && award.name == 'Blue First' && award.quality < 50 then award.quality += 1
       when award.expires_in < 0 && award.name == 'Blue Compare' then award.quality = award.quality - award.quality
       when award.expires_in < 0 && award.name == 'NORMAL ITEM' && award.quality > 0 then award.quality -= 1
+      when award.expires_in < 0 && award.name == 'Blue Star' && award.quality > 0 then award.quality -= 2
     end
   end
 end
