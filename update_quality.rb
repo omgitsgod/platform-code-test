81606a3a47414ed309739ff6a674fb3913448128
omgitsgod <omgitsgod@gmail.com>
2019-07-01 22:58:56 -0400

    Add another case and if statement

--- update_quality.rb
+++ update_quality.rb
@@ -6,18 +6,23 @@ def update_quality(awards)
     when award.name == 'NORMAL ITEM' && award.quality > 0 then award.quality -= 1
     when award.name == 'Blue Star' && award.quality > 0 then award.quality -= 2
     when award.name == 'Blue First' && award.quality < 50 then award.quality += 1
-    when award.name == 'Blue Compare' && award.expires_in > 10 && award.quality < 50 then award.quality += 1
-    when award.name == 'Blue Compare' && award.expires_in < 6 && award.quality < 50 then award.quality += 3
-    when award.name == 'Blue Compare' && award.expires_in < 11 && award.quality < 50 then award.quality += 2
+    when award.name == 'Blue Compare'
+      case
+        when award.expires_in > 10 && award.quality < 50 then award.quality += 1
+        when award.expires_in < 6 && award.quality < 50 then award.quality += 3
+        when award.expires_in < 11 && award.quality < 50 then award.quality += 2
+      end
     end
     case
     when award.name != 'Blue Distinction Plus' then award.expires_in -= 1
     end
-    case
-    when award.expires_in < 0 && award.name == 'NORMAL ITEM' && award.quality > 0 then award.quality -= 1
-    when award.expires_in < 0 && award.name == 'Blue Star' && award.quality > 0 then award.quality -= 2
-    when award.expires_in < 0 && award.name == 'Blue First' && award.quality < 50 then award.quality += 1
-    when award.expires_in < 0 && award.name == 'Blue Compare' then award.quality = award.quality - award.quality
+    if award.expires_in < 0
+      case
+      when award.name == 'NORMAL ITEM' && award.quality > 0 then award.quality -= 1
+      when award.name == 'Blue Star' && award.quality > 0 then award.quality -= 2
+      when award.name == 'Blue First' && award.quality < 50 then award.quality += 1
+      when award.name == 'Blue Compare' then award.quality = award.quality - award.quality
+      end
     end
   end
 end
